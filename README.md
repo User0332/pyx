dcf2d2e05a70418fccc15af7b3aa3f79c87143f9
User0332 <carlzfurtado@gmail.com>
2022-10-10 20:34:56 -0400

    make project creator

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Python XML
 
-Kind of like JSX but with Python. This repository will also include `pysite`, a library to be used with PYX and Flask, basically filling in the place of React. Create your project in `test_proj`, with app.pyx being your main application. Then, with the root directory of the repo added to PYTHONPATH, `src/` added to PATH, and Flask installed, run the command `python -m pysite run` to deploy your app using Flask.
+Kind of like JSX but with Python. This repository will also include `pysite`, a library to be used with PYX and Flask, basically filling in the place of React. Create your project in `test_proj`, with app.pyx being your main application. Then, with the root directory of the repo added to PYTHONPATH, `src/` added to PATH, and Flask installed, run the command `python -m pysite run` inside of your project directory to deploy your app using Flask. If you want to create a new project, use the command `python -m pysite new {projectname}`.
--- pysite/__main__.py
+++ pysite/__main__.py
@@ -20,3 +20,31 @@ if sys.argv[1] == "run":
 
 	try: os.system("flask run")
 	except KeyboardInterrupt: pass
+
+if sys.argv[1] == "new":
+	if len(sys.argv) < 3:
+		print("Missing project name!")
+		exit(1)
+
+	proj_name = sys.argv[2]
+
+	try:
+		os.mkdir(proj_name)
+		os.mkdir(f"{proj_name}/components")
+		os.mkdir(f"{proj_name}/snippets")
+
+		with open(f"{proj_name}/app.pyx", 'w') as f:
+			f.write(
+				'import pysite\nfrom flask import Flask\nfrom pysite.tags '
+				'import *\n\napp = Flask(__name__)\n\n@app.route(\'/\')\n'
+				'def index():\n\treturn (\n\t\t<pyx>\n\t\t\t<h1>'
+				'Hello World!</h1>\n\t\t\t<br></br>\n\t\t\t<p '
+				'style="display:inline-block;">This site was built with</p>'
+				'\n\t\t\t<a href="https://github.com/User0332/pyx">PySite'
+				'</a>\n\t\t</pyx>\n\t).html'
+			)
+
+		open(f"{proj_name}/components/__init__.py", 'w').close()
+		open(f"{proj_name}/snippets/__init__.py", 'w').close()
+	except OSError as e:
+		print(f"An error occurred: {e}")
