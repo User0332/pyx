6c06c4df8e0f5820fa6b05b318dfaa1acba9fee8
User0332 <carlzfurtado@gmail.com>
2023-05-26 13:03:00 -0400

    upload to pypi

--- pyx-pysite/pysite/__init__.py
+++ pyx-pysite/pysite/__init__.py
@@ -1,12 +1,5 @@
-"""
-Pysite is a Python web framework, inspired by JSX. It uses Flask in the backend.
-Common commands for the `pysite` CLI are `new <projectname>` and `run`.
-"""
-
 import js2py
 import inspect
 from . import tags
 from typing import Callable
 from flask import Flask
-
-__version__ = "1.0.0"
